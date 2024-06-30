caf80abd91ff39b2947f6ff0fbc6c4cba4c369e5
Oleg <sirov@ukr.net>
2024-07-01 01:12:35 +0300

    add film search

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './SharedLayout';
 import Home from '../pages/Home';
 
-// const Movies = lazy(() => import('pages/Movies'));
+const Movies = lazy(() => import('../pages/Movies'));
 // const MoviesDetails = lazy(() => import('pages/MoviesDetails'));
 // const Cast = lazy(() => import('./Cast'));
 // const Reviews = lazy(() => import('./Reviews'));
@@ -14,8 +14,9 @@ const App: React.FC = () => {
   return (
     <Routes>
         <Route path="/" element={<SharedLayout />}>
-          <Route index element={<Home />} />
-          {/* <Route path="/movies" element={<Movies />} />
+        <Route index element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+          {/* 
           <Route path="/movies/:id" element={<MoviesDetails />}>
             <Route path="cast" element={<Cast />} />
             <Route path="reviews" element={<Reviews />} />
---
+++ src/pages/Movies/Movies.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
+import { Link, useLocation, useSearchParams } from "react-router-dom";
+import { SearchHeader, Form, Button, Input } from "./Movies.styled";
+import { getSearchMovies } from "../../services/content-api";
+import { Loading } from "notiflix/build/notiflix-loading-aio";
+
+import {
+  Movie,
+  SearchMoviesResponse
+} from "../../models";
+
+const Movies: React.FC = () => {
+  const [query, setQuery] = useState<string>("");
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [getted, setGetted] = useState<boolean>(false);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const queryS = searchParams.get("query") ?? "";
+
+  useEffect(() => {
+    if (queryS) {
+      getSearchMovies(queryS).then((moveis: SearchMoviesResponse) => {
+        setMovies(moveis.results);
+      });
+    }
+  }, [queryS]);
+
+  const location = useLocation();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    
+    const nextParams: any = query !== "" ? { query } : {};
+    setSearchParams(nextParams);
+    Loading.circle("Loading...");
+    getSearchMovies(query).then((moveis: SearchMoviesResponse) => {
+      setMovies(moveis.results);
+      setGetted(true);
+    });
+    setQuery("");
+    Loading.remove();
+  };
+
+  return (
+    <>
+      <SearchHeader>
+        <Form onSubmit={handleSubmit}>
+          <Button type="submit"></Button>
+
+          <Input
+            onChange={handleChange}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search movies"
+            name="query"
+            value={query}
+            required
+          />
+        </Form>
+      </SearchHeader>
+      {getted && movies.length === 0 && (
+        <p>We don't have any movies for this query</p>
+      )}
+      <ul>
+        {movies.length > 1 &&
+          movies.map(({ id, title }) => (
+            <li key={id}>
+              <Link to={`/movies/${id}`} state={{ from: location }}>
+                {title}
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+};
+export default Movies;
