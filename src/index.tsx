e38e34355030eed54d39cca712815730ce025959
Oleg <sirov@ukr.net>
2024-07-24 18:07:59 +0300

    edit forgot link

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./redux/auth/interceptors";
@@ -17,11 +17,9 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <React.StrictMode>
-        <HashRouter>
-          <BrowserRouter basename={basename}>
-            <App />
-          </BrowserRouter>
-        </HashRouter>
+        <BrowserRouter basename={basename}>
+          <App />
+        </BrowserRouter>
       </React.StrictMode>
     </PersistGate>
   </Provider>
