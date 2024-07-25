7a4e038552f524084d2d1fd9307b6a218a936ebd
Oleg <sirov@ukr.net>
2024-07-25 22:48:19 +0300

    add baseURL

--- src/redux/auth/auth-operations.ts
+++ src/redux/auth/auth-operations.ts
@@ -10,8 +10,8 @@ import {
 import { RootState } from "../../store";
 import { Notify } from "notiflix/build/notiflix-notify-aio";
 
-export const baseURL = "http://localhost:3000";
-// export const baseURL = "https://ts-nodejs-5beg.onrender.com";
+// export const baseURL = "http://localhost:3000";
+export const baseURL = "https://ts-nodejs-5beg.onrender.com";
 
 axios.defaults.baseURL = baseURL;
 
